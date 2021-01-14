b71b1514f51cecb277620f73b773db4c5157b44d
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-01-14 09:39:19 -0300

    aula sobre str e aspas

---
+++ Aula2/Aula2.py
@@ -0,0 +1,6 @@
+#  str - string
+print('É um "texto" - (str')
+print("É um 'texte' - (str)")
+print('É um \"texto\" - (str)')
+print("É um \'texto\' - (str)")
+print(r'Esse é meu \n') #  r - raw, executa o texto como está na função print
