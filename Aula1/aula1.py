09e5ee099ec283f38cade6ea17e24647c3441f24
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-01-14 09:01:59 -0300

    aula 1

--- Aula1/aula1.py
+++ Aula1/aula1.py
@@ -1,2 +1,9 @@
-print('Teste Commit')
-print('Teste 2')
+""""
+A função sep= separa os elementos por determinado elemento, Ex: sep='-', separa por - e não mais por espaços
+A função end= adiciona algo ao final da linha, se deixado vazio: end='' retira a quebra de linha padrão do print
+"""
+print('Gabriel', 'Rodrigo', sep='-', end='')
+print('João', 'e', 'Maria', sep='-')
+#  print('824.176.070-18')
+print('824', '176', '070', sep='.', end='-')
+print('18')
