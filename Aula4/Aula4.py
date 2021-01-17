ca0292d0097a146ede13788e1b72bbf112916daf
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-01-17 15:34:43 -0300

    Operadores aritiméticos

---
+++ Aula4/Aula4.py
@@ -0,0 +1,15 @@
+'''
++, -, *, /, //, **, %, ()
+// - Divisão inteira, arredonda resultado
+** - Porenciação, eleva um número ao outro
+% - resultado de divisão
+() - Altera a ordem de fazer a operação
+'''
+print('Adição 10 + 10:', 10 + 10)  # Adição
+print('Multiplicação 10 * 10:', 10 * 10)  # Multiplicação
+print('Potenciação 10**10:', 10**10)
+print('Multiplicação de tipos:', 10 * 'Gabriel')
+print('Subtração 10 - 10:', 10-10)  # Subtração
+print('Divisão 10/10:', 10 / 10)
+print('Divisão inteira 15.5//10:', 15.5 // 10)
+print('Resto de divisão(Modulo) 15%10:', 15 % 10)
