371e497a8ae8c0ec55e31e62d03608c45805afbd
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-01-17 15:17:43 -0300

    dados primitivos

--- Aula3/Aula3.py
+++ Aula3/Aula3.py
@@ -9,3 +9,11 @@ print('Gabriel', type('Gabriel'))
 print(10, type(10))
 print(90.83, type(90.83))
 print(100 == 100, type(100 == 100))
+print('10 + 10:', 10 + 10)  # Soma porque são int
+print('10 + 10:' ' 10' + '10')  # Concatena porque são str
+print('-'*30)
+# Pequeno exercício
+print('Nome: Gabriel', type('Gabriel'))
+print(21, type(21))
+print(1.76, type(1.76))
+print(21 > 18, type(21 > 18))
