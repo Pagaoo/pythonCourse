17e9bf35446b740b4a6070c405cd1c22ace934af
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-01-17 15:08:35 -0300

    dados primitivos

---
+++ Aula3/Aula3.py
@@ -0,0 +1,11 @@
+'''
+Tipos de dados primitivos:
+str - string - textos
+int - inteiros - 12345
+float - real/flutuante - 1.5; 0.6; -1.6
+bool - booleano/logico - True or False
+'''
+print('Gabriel', type('Gabriel'))
+print(10, type(10))
+print(90.83, type(90.83))
+print(100 == 100, type(100 == 100))
