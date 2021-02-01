5c44984572e0601fba45595b39195690fd41d189
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-02-01 18:58:45 -0300

    Exercicios Propostos

--- ExerciciosPropostos/Exercicio2.py
+++ ExerciciosPropostos/Exercicio2.py
@@ -3,3 +3,20 @@ Faça um programa que pergunte a hora ao usuário e, baseando-se no horário des
 apropriada. Ex.
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23
 """
+import time, datetime
+
+hora = input('Digite a hora (0 - 23): ')
+
+if hora.isdigit():
+    hora = int(hora)
+    if hora < 0 or hora > 23:
+        print('Digite a hora entre 0 - 23')
+    else:
+        if hora <= 11:
+            print('Bom dia!')
+        elif hora <= 17:
+            print('Boa tarde!')
+        else:
+            print('Boa noite!')
+else:
+    print('Digite um número entre 0 - 23')
