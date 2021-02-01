6dd2592ff48c992a4a88cd9377c30096778331e0
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-02-01 18:46:40 -0300

    Exercicios Propostos

--- ExerciciosPropostos/Exercicio1.py
+++ ExerciciosPropostos/Exercicio1.py
@@ -2,3 +2,15 @@
 Faça um programa que peça ao usuário para digitar um número inteiro, informe se este número é par ou ímpar.
 Caso o usuário não digite um número inteiro, informe que não é um número inteiro.
 """
+
+num = input('Digite um número inteiro: ')
+
+if num.isdigit():
+    num = int(num)
+    if num % 2 == 0:
+        print(f'O {num} é um número par')
+    else:
+        print(f'O {num} é um número impar')
+else:
+    print('É um número não inteiro ou uma letra. Insira novamente')
+
