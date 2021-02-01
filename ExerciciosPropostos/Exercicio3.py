f05b625028c9b39b713e9013211b4444ba2c31b7
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-02-01 10:30:08 -0300

    Exercicios Propostos

---
+++ ExerciciosPropostos/Exercicio3.py
@@ -0,0 +1,5 @@
+"""
+Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou men, escreva:
+'Seu nome é curto'; se tiver entre 5 e 6 letras, escreva: 'Seu nome é normal'; se maior que 6, escreva:
+'Seu nome é muito grande'
+"""
