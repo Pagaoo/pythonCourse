6e1d8955d14a41f4015957236a69c1ea3269cbad
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-02-01 19:16:54 -0300

    Exercicios Propostos

--- ExerciciosPropostos/Exercicio3.py
+++ ExerciciosPropostos/Exercicio3.py
@@ -1,5 +1,14 @@
 """
-Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou men, escreva:
+Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou menos, escreva:
 'Seu nome é curto'; se tiver entre 5 e 6 letras, escreva: 'Seu nome é normal'; se maior que 6, escreva:
 'Seu nome é muito grande'
 """
+primeiro_nome = input('Digite seu primeiro nome: ')
+tamanho = len(primeiro_nome)
+
+if tamanho <= 4:
+    print(f'O nome {primeiro_nome} é curto')
+elif 5 <= tamanho <= 6:
+    print(f'O nome {primeiro_nome} é normal')
+else:
+    print(f'O nome {primeiro_nome} é muito grande')
