6b90a2be2d70084b5e8923b5c94d292be7835357
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-02-04 19:41:45 -0300

    Formatando strings

---
+++ Aula5/Formatando_str.py
@@ -0,0 +1,28 @@
+"""
+Formatando valores com modificadores
+
+:s - Textos (str)
+:d - Inteiros (int)
+:f - Números de ponto flutuante (float)
+:.(NÚMERO)f - Quantidade de casas decimais (float)
+:(CARACTERE) (> ou < ou ^) (QUANTIDADE) (TIPO - s, d ou f)
+
+> - Esquerda
+< - Direita
+^ - Centro
+"""
+
+num_1 = 10
+num_2 = 3
+divisao = num_1 / num_2
+print(divisao)
+print(f'{divisao:.2f}')
+print(f'{num_1:0>9}')  # Adiciona casas até ter 9 casas, incluindo o número selecionado
+print(f'{num_1:0>9.2f}')  # Terão 9 casas e as últimas serão separadas por ponto
+print(f'{num_2:9>9}')
+print(f'{num_2:9<9}')
+print(f'{num_2:9^9}')
+print('-'*50)
+
+nome = 'Gabriel'
+print(f'{nome:#^50}')
