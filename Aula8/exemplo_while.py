ddde9de6c6f61af88ca225cf72658439259f9674
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-02-22 11:40:24 -0300

    Exemplo com While

---
+++ Aula8/exemplo_while.py
@@ -0,0 +1,27 @@
+operacao = True
+
+while operacao:
+    num_1 = input('Digite um número: ')
+    num_2 = input('Digite um segundo número: ')
+    operador = input('Escolha um operador: ')  # Apenass +, -, /, *
+
+    if not num_1.isnumeric() or not num_2.isnumeric():
+        print('Você precisa digitar somente números')
+        continue
+
+    num_1 = int(num_1)
+    num_2 = int(num_2)
+
+    if operador == '+':
+        print(num_1 + num_2)
+    elif operador == '-':
+        print(num_1 - num_2)
+    elif operador == '*':
+        print(num_1 * num_2)
+    elif operador == '/':
+        print(num_1 / num_2)
+    else:
+        print('Operador inválido')
+
+    operacao = False
+print('Saiu da calculadora')
