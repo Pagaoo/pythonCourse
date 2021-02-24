53c43136abdd5ecd0b40fb63b574d9928dc6754f
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-02-24 17:47:35 -0300

    iterando com while

--- Aula8/iterando-while.py
+++ Aula8/iterando-while.py
@@ -1,5 +1,21 @@
 frase = 'o rato roeu a roupa do rei de roma'
 tamanho_frase = len(frase)  # Vai contar os espaços como indexes
-
 contador = 0
-print(frase[5])
+nova_frase = ''
+
+print(frase)
+input_usuario = input('Qual letra da frase acima você quer deixar maiúscula: ')
+
+while contador < tamanho_frase:
+    if frase[contador] == ' ':
+        print('Espaço', contador)
+    else:
+        print(frase[contador], contador)
+
+    if frase[contador] == input_usuario:
+        nova_frase += input_usuario.upper()
+    else:
+        nova_frase += frase[contador]
+
+    contador += 1
+print(nova_frase)
